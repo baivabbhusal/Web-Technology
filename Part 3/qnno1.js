3f4259ff9f2367d329a84676ace3a16b93edb054
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-02-16 23:20:32 +0545

    js files added

---
+++ Part 3/qnno1.js
@@ -0,0 +1,10 @@
+var age=30; //global variable
+const name="baivab"; //const variable needs to be initialized
+let isStudent; //block scope variable
+isStudent=true;
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(isStudent));
+console.log(age);
+console.log(name);
+console.log(isStudent);
---
+++ Part 3/qnno6.js
@@ -0,0 +1,5 @@
+let sum=0;
+for(i=2;i<=200;i=i+2){
+    sum=sum+i;
+ }
+ console.log("The sum of even Numbers from 1 to 200 is: "+sum);
