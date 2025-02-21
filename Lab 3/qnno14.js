e33750723866c4908a08b523606148c62156229b
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-02-21 23:42:54 +0545

    Add uncommited files

--- Lab 3/qnno14.js
+++ Lab 3/qnno14.js
@@ -4,10 +4,12 @@ const buttonLength=document.querySelector("#length");
 buttonLength.addEventListener("click",()=>{
 console.log(firstSentence.value.length);
 });
+//convert to upper case
 const buttobUpper=document.querySelector("#upper");
 buttobUpper.addEventListener("click",()=>{
 console.log(firstSentence.value.toUpperCase());
 });
+//convert to lower case
 const buttonLower=document.querySelector("#lower");
 buttonLower.addEventListener("click",()=>{
 console.log(firstSentence.value.toLowerCase());
@@ -15,9 +17,16 @@ console.log(firstSentence.value.toLowerCase());
 //concatenate
 const firstName=document.querySelector("#first-name");
 const lastName=document.querySelector("#last-name");
-const buttonConcate=document.querySelector("#concate");
+const buttonConcate=document.querySelector("#concat");
 buttonConcate.addEventListener("click",()=>{
 console.log(firstName.value.concate(lastName.value));
 });
-const secondSentence=document.querySelector("#second-sentence");
+//slice
+const startIndex=document.querySelector("#start-index");
+const endIndex=document.querySelector("#end-index");
+const buttonSlice=document.querySelector("#slice");
+buttonSlice.addEventListener("click",()=>{
+console.log(secondSentence.value.slice(startIndex.value,endIndex.value));
+});
+
 
