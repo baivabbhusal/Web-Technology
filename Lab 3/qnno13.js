37de300dd026baaeb164748301ca2ca7e3c501d9
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-02-22 23:46:21 +0545

    Question no 13 added

---
+++ Lab 3/qnno13.js
@@ -0,0 +1,5 @@
+let sentence="This is javascript of web development.";
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+console.log(sentence.slice(5,10));
