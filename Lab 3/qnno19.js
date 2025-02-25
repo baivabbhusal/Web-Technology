d5298d9a4ee3bb08f174a60f115168bf0e41fada
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-02-25 21:32:51 +0545

    add new files

---
+++ Lab 3/qnno19.js
@@ -0,0 +1,12 @@
+const array=["Apple","Ball",23,2.34]
+console.log(array);
+array.push("Ram");
+console.log(array);
+array.pop();
+console.log(array);
+console.log(array.length)
+console.log(array.sort());
+array.unshift("hello");
+console.log(array);
+array.shift();
+console.log(array);
--- Lab 3/qnno24.js
+++ Lab 3/qnno24.js
@@ -1,4 +1,11 @@
 const sentence="Web programming is fun to learn but at the same time programming is difficult too. Web programming is core course in BIM third semester";
-for (let i of sentence){
+
+const sentenceToArray=sentence.split();
+var wordCount=0;
+for(let i=0;i<=sentenceToArray.length;i++){
+    // if(i=="programming"){
+    //     wordCount+=1;
+    // }
     console.log(i);
 }
+console.log("The word count of programming is "+wordCount);
